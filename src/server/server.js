5c30513e0f888a14ddb0657376fec28e1a68c48f
cskupie <Christian.skupien@daimler.com>
2021-03-02 14:01:32 +0100

    change port

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 const app = require('../server/app')
 const port = 80
 
-app.listen(port, () =>
-  console.log(`Example app listening on port http://localhost:${port}/ !\n`)
+app.listen(process.env.PORT, () =>
+  console.log(
+    `Example app listening on port http://localhost:${process.env.PORT}/ !\n`
+  )
 )
